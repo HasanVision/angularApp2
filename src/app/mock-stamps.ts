1391f8490e5cf4c3492ae0a1996f0cb346cc2812
HasanVision <hasan.visionx@gmail.com>
2024-11-13 14:16:48 -0500

    disable add to cart button for out of stock

--- src/app/mock-stamps.ts
+++ src/app/mock-stamps.ts
@@ -1,10 +1,10 @@
 export const STAMPS = [
     { id: 1, name: 'Vintage Stamp', price: 2.99, image: 'assets/stamp_1.png', stock: 0 },
-    { id: 2, name: 'Rare Stamp', price: 5.49, image: 'assets/stamp_2.png'},
+    { id: 2, name: 'Rare Stamp', price: 5.49, image: 'assets/stamp_2.png', stock: 4},
     { id: 3, name: 'Collector’s Edition', price: 7.99, image: 'assets/stamp_3.png', stock: 0},
-    { id: 4, name: 'Commemorative Stamp', price: 3.49, image: 'assets/stamp_4.png' },
+    { id: 4, name: 'Commemorative Stamp', price: 3.49, image: 'assets/stamp_4.png', stock: 4 },
     { id: 5, name: 'Limited Edition 1', price: 6.99, image: 'assets/stamp_5.png', stock: 0},
-    { id: 6, name: 'Limited Edition 2', price: 6.99, image: 'assets/stamp_6.png' },
-    { id: 7, name: 'Limited Edition 3', price: 6.99, image: 'assets/stamp_7.png' },
-    { id: 8, name: 'Limited Edition 4', price: 6.99, image: 'assets/stamp_8.png' },
+    { id: 6, name: 'Limited Edition 2', price: 6.99, image: 'assets/stamp_6.png', stock: 42 },
+    { id: 7, name: 'Limited Edition 3', price: 6.99, image: 'assets/stamp_7.png', stock: 43 },
+    { id: 8, name: 'Limited Edition 4', price: 6.99, image: 'assets/stamp_8.png', stock: 5 },
   ];
